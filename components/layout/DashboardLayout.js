f9477727ba1888d9e450a9787fceecc17b3b9aec
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2023-02-17 16:30:12 +0600

    Change responsive height issue

--- components/layout/DashboardLayout.js
+++ components/layout/DashboardLayout.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react'
 import DashboardHeader from '../common/DashboardHeader'
 import LeftNav from '../common/LeftNav'
 
+import styles from "../../styles/dashboard/Dashboard.module.css"
+
 const DashboardLayout = ({ children }) => {
 
     const [mobileMenu, setMobileMenu] = useState(false)
 
     return (
         <>
-            <div style={{ background: "#F1F1F1", minHeight: "100vh", padding: "15px", display: "flex", justifyContent: "center" }}>
+            <div style={{ background: "#F1F1F1", minHeight: "100vh", padding: "15px", display: "flex", justifyContent: "center" }} className={styles.dashboardWrapper}>
                 <div style={{ boxShadow: "0 0 5px rgba(0, 0, 0, 0.5)", background: "#FFFFFF", borderRadius: "25px", width: "100%", display: "flex", overflow: "hidden", flex: "1" }}>
                     <LeftNav mobileMenu={mobileMenu} />
                     <div className='flex-1 w-100'>
