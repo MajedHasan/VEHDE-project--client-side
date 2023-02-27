9cd05a08f20662e45b812db20fa873a609a37aab
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2023-02-27 21:41:37 +0600

    Messages part 90% done

--- pages/dashboard/VEHDEScore.js
+++ pages/dashboard/VEHDEScore.js
@@ -11,6 +11,12 @@ import { useState } from "react"
 import EarningTable from "../../components/payment/EarningTable"
 import EarningDetailTable from "../../components/payment/EarningDetailTable"
 import { fullYear, monthNames } from "./Payment"
+import Image from "next/image"
+
+import CashOutImg from "../../assets/img/dashboard/VEHDEScore/CashOut.png"
+import SuccessImg from "../../assets/img/dashboard/VEHDEScore/CashOutSuccess.png"
+import EmptyBalanceImg from "../../assets/img/dashboard/VEHDEScore/EmptyBalance.png"
+import InfoImg from "../../assets/img/dashboard/VEHDEScore/info.png"
 
 
 const VEHDEScore = () => {
@@ -26,22 +32,115 @@ const VEHDEScore = () => {
         ["Mar 18, 2022", "85", "Rental", "$85.00", "235"],
     ]
     const [totalScore, setTotalScore] = useState(100)
+    const [cashoutAmount, setCashoutAmount] = useState(100)
+
+    const handleCashoutAmount = (type) => {
+        if (type === "descrease") {
+            if (cashoutAmount > 1) setCashoutAmount(cashoutAmount - 1)
+        }
+        else if (type === "increase") {
+            if (cashoutAmount < totalScore) setCashoutAmount(cashoutAmount + 1)
+        }
+    }
+
+    const handleCashOut = () => {
+        if (cashoutAmount > totalScore) return setCashOut("empty")
+        setTotalScore(totalScore - cashoutAmount)
+        setCashOut("success")
+        setTimeout(() => {
+            setCashOut(null)
+        }, 1500);
+    }
 
     return (
         <>
             <DashboardLayout>
                 <div className={styles.container}>
                     <DashboardBanner leftContent={<DashboardBannerLeftContent page="VEHDEScore" setViewScore={setViewScore} setCashOut={setCashOut} />} rightContent={<DashboardBannerRightContent page="VEHDEScore" />} />
+                    <div className={styles.infoWrapper}>
+                        <div>
+                            <div>
+                                <h2>{totalScore}</h2>
+                                <p>Score earned</p>
+                            </div>
+                            <div>
+                                <Image src={InfoImg} alt="" />
+                            </div>
+                            <div>
+                                <h2>5/22</h2>
+                                <p>Redeemable date</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <h2>Varies</h2>
+                                <p>Rental equivalent</p>
+                            </div>
+                            <div>
+                                <h2>120</h2>
+                                <p>Dollar equivalent</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </DashboardLayout>
             {
                 cashOut && <ModalMd setModal={setCashOut}>
                     {
-                        totalScore && <>
+                        totalScore !== 0 && cashOut !== "empty" && <>
+                            {
+                                cashOut !== "success" && <div className={styles.modalMd}>
+                                    <h5>VEHDE Score Balance</h5>
+                                    <Image src={CashOutImg} alt="" />
+                                    <div>
+                                        <h2>VEHDE Score | History</h2>
+                                        <p>Please indicate the VEHDEScore amount you want to cash out.</p>
+                                        <div className={styles.inputGroup}>
+                                            <label htmlFor="">Enter your amount</label>
+                                            <div>
+                                                <i className="fa fa-minus" onClick={() => handleCashoutAmount("descrease")}></i>
+                                                <input type="text" placeholder="$100" onChange={(e) => setCashoutAmount(e.target.value)} value={cashoutAmount} />
+                                                <i className="fa fa-plus" onClick={() => handleCashoutAmount("increase")}></i>
+                                            </div>
+                                        </div>
+                                        <button className="my-btn" onClick={handleCashOut}>Cash Out</button>
+                                    </div>
+                                </div>
+                            }
                         </>
                     }
                     {
-                        !totalScore && <>
+                        cashOut === "success" && <div className={styles.modalMd}>
+                            <Image src={SuccessImg} alt="" />
+                            <div>
+                                <h2>VEHDE Score | Cash Out</h2>
+                                <p>Congratulations! You have successfully cashed out your VEHDE Score equivalent $100. This amount will be available in your active payment account within the next 48 hours.</p>
+                                <h4>Live VEHDE, keep earning!</h4>
+                            </div>
+                        </div>
+                    }
+                    {
+                        !totalScore && cashOut !== "success" && <>
+                            <div className={styles.modalMd}>
+                                <Image src={EmptyBalanceImg} alt="" />
+                                <div>
+                                    <h2>VEHDE Score | Cash</h2>
+                                    <p>Sorry, you do not have enough VEHDE Score at this moment to cash out. Please keep earning more VEHDE Score.</p>
+                                    <h4>Stay VEHDE, keep earning!</h4>
+                                </div>
+                            </div>
+                        </>
+                    }
+                    {
+                        cashOut === "empty" && <>
+                            <div className={styles.modalMd}>
+                                <Image src={EmptyBalanceImg} alt="" />
+                                <div>
+                                    <h2>VEHDE Score | Cash</h2>
+                                    <p>Sorry, you do not have enough VEHDE Score at this moment to cash out. Please keep earning more VEHDE Score.</p>
+                                    <h4>Stay VEHDE, keep earning!</h4>
+                                </div>
+                            </div>
                         </>
                     }
                 </ModalMd>
