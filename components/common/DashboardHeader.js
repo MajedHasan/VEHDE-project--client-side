e1548aea6c58070765149e096029103e9e4538c6
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2023-03-30 19:56:07 +0600

    96% revisions are done

--- components/common/DashboardHeader.js
+++ components/common/DashboardHeader.js
@@ -7,6 +7,7 @@ import LogOutImg from "../../assets/img/dashboard/logout.svg"
 import Link from "next/link"
 import { useState } from "react"
 import ModalLg from "../../components/common/ModalLg"
+import ModalMd from "../../components/common/ModalMd"
 
 import ProfileModalImg1 from "../../assets/img/dashboard/MyRenter/ProfileModalImg1.svg"
 import ProfileModalImg2 from "../../assets/img/dashboard/MyRenter/ProfileModalImg2.png"
@@ -22,6 +23,10 @@ const DashboardHeader = ({ mobileMenu, setMobileMenu, userRole }) => {
     const [available, setAvailable] = useState(true)
     const [mobileSearch, setMobileSearch] = useState(false)
     const [showProfileModal, setProfileModal] = useState(false)
+
+    const [showModalLg, setShowModalLg] = useState(false)
+    const [showModalMd, setShowModalMd] = useState(false)
+    const [editLicense, setEditLicense] = useState(false)
 
     return (
         <>
@@ -99,14 +104,14 @@ const DashboardHeader = ({ mobileMenu, setMobileMenu, userRole }) => {
                     <div className={styles.profileModal}>
                         <h2>My Profile</h2>
                         <div>
-                            <Link href="">
+                            <div onClick={() => setShowModalMd("auto")}>
                                 <Image src={ProfileModalImg1} alt="" />
                                 <h4>Auto insurance</h4>
-                            </Link>
-                            <Link href="">
+                            </div>
+                            <div onClick={() => setShowModalLg("license")}>
                                 <Image src={ProfileModalImg2} alt="" />
                                 <h4>Driver’s license</h4>
-                            </Link>
+                            </div>
                             <Link href="/dashboard/Payment/PaymentMethod">
                                 <Image src={ProfileModalImg3} alt="" />
                                 <h4>Payment method</h4>
@@ -115,17 +120,166 @@ const DashboardHeader = ({ mobileMenu, setMobileMenu, userRole }) => {
                                 <Image src={ProfileModalImg4} alt="" />
                                 <h4>VEHDEPass Membership</h4>
                             </Link>
-                            <Link href="">
+                            <div onClick={() => setShowModalLg("contact")}>
                                 <Image src={ProfileModalImg5} alt="" />
                                 <h4>Contact Information</h4>
-                            </Link>
-                            <Link href="">
+                            </div>
+                            <div onClick={() => setShowModalMd("password")}>
                                 <Image src={ProfileModalImg6} alt="" />
                                 <h4>Change Password</h4>
-                            </Link>
+                            </div>
                         </div>
                     </div>
                 </ModalLg>
+            }
+            {
+                showModalLg === "license" && <ModalLg setModal={setShowModalLg}>
+                    <div className={styles.modalLgBox}>
+                        <h2>Driver's license
+                            <span>JW-H (TX) 868746-78613</span>
+                        </h2>
+                        <h5>Status:
+                            <span> Active</span>
+                        </h5>
+                        <p>Your driver's license is currently <span>active</span> and <span>approved</span>. You can revise your information by selecting edit icon, make the changes, and select OK to save. Please allow up to 24hours for your new information to be reviewed for approval.
+                            <br />
+                            <br />
+                            (NOTE: To change your name please submit a separate SNOW Ticket.)
+                            <span></span>
+                        </p>
+                        <div>
+                            <i className="fa fa-pencil" onClick={() => setEditLicense(true)}></i>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6 col-12 pe-md-1 pe-0 mb-2">
+                                <div className={styles.modalInputBox}>
+                                    <label htmlFor="">First name</label>
+                                    <input type="text" value={"Jane 🔒"} readOnly />
+                                </div>
+                            </div>
+                            <div className="col-md-6 col-12 ps-md-1 ps-0 mb-2">
+                                <div className={styles.modalInputBox}>
+                                    <label htmlFor="">Last name</label>
+                                    <input type="text" value={"Wesley 🔒"} readOnly />
+                                </div>
+                            </div>
+                            <div className="col-md-6 col-12 pe-md-1 pe-0 mb-2">
+                                <div className={styles.modalInputBox}>
+                                    <label htmlFor="">Country</label>
+                                    <input type="text" value={"United States of America 🔒"} readOnly />
+                                </div>
+                            </div>
+                            <div className="col-md-6 col-12 ps-md-1 ps-0 mb-2">
+                                <div className={styles.modalInputBox}>
+                                    <label htmlFor="">State</label>
+                                    <select name="" id="">
+                                        <option value="">Texas</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="col-12 mb-2">
+                                <div className={styles.modalInputBox}>
+                                    <label htmlFor="">License number</label>
+                                    <input type="text" placeholder="613686990" />
+                                </div>
+                            </div>
+                            <div className="col-md-6 col-12 pe-md-1 pe-0 mb-2">
+                                <div className={styles.modalInputBox}>
+                                    <label htmlFor="">Experiation date</label>
+                                    <select name="" id="">
+                                        <option value="">August</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="col-md-3 col-12 px-md-1 px-0 mb-2">
+                                <div className={styles.modalInputBox}>
+                                    <label htmlFor="">Day</label>
+                                    <input type="text" placeholder="04" />
+                                </div>
+                            </div>
+                            <div className="col-md-3 col-12 ps-md-1 ps-0 mb-2">
+                                <div className={styles.modalInputBox}>
+                                    <label htmlFor="">Year</label>
+                                    <select name="" id="">
+                                        <option value="">2026</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        {
+                            editLicense && <><div className={styles.modalCheckbox}>
+                                <input type="checkbox" name="" id="" />
+                                <label htmlFor="">I agree to VEHDE's <Link href="">Terms of Use</Link> and acknowledge that i have read the <a href="">Privacy Notice.</a></label>
+                            </div>
+                                <div className={styles.modalBtnBox}>
+                                    <button className={styles.modalBtnSuccess} onClick={() => { setEditLicense(false); setShowModalLg(false) }}>OK</button>
+                                    <button className={styles.modalBtnSuccessOutline} onClick={() => { setEditLicense(false); setShowModalLg(false) }}>Cancel</button>
+                                </div>
+                            </>}
+                    </div>
+                </ModalLg>
+            }
+            {
+                showModalMd === "auto" && <ModalMd setModal={setShowModalMd}>
+                    <div className={`${styles.modalLgBox}`}>
+                        <h2>VEHDE
+                            <span>Auto Insurance</span>
+                        </h2>
+                        <p style={{ marginTop: "70px", fontSize: "14px", lineHeight: 1, marginBottom: "5px" }}>VEHDE makes protection plans available to guests. Details of the protection plans can be found by reviewing</p>
+                        <p style={{ fontSize: "12px", color: "#6A6A6A" }}>Understanding and choosing ride auto protection.</p>
+
+                        <p style={{ marginTop: "30px", fontSize: "14px", lineHeight: 1, marginBottom: "5px" }}>VEHDE makes protection plans available to guests. Details of the protection plans can be found by reviewing</p>
+                        <p style={{ fontSize: "12px", color: "#6A6A6A" }}>Understanding and choosing ride auto protection.</p>
+
+                        <div className="mt-5">
+                            <button className="my-btn mx-auto">Add-On</button>
+                        </div>
+                    </div>
+                </ModalMd>
+            }
+            {
+                showModalMd !== "auto" && showModalMd !== null && showModalMd !== false && <ModalMd setModal={setShowModalMd}>
+                    {showModalMd === "password" && <div className={styles.modalLgBox}>
+                        <h2>VEHDE
+                            <span>Change Password</span>
+                        </h2>
+                        <div className={`${styles.modalInputBox} mt-5`}>
+                            <label htmlFor="">VEHDE Password</label>
+                            <input type="text" placeholder="Enter current password" />
+                        </div>
+                        <div className={`${styles.modalInputBox} mt-3 mb-3`}>
+                            <label htmlFor="">New password</label>
+                            <input type="text" placeholder="Enter new password" />
+                        </div>
+                        <div></div>
+                        <div className={`${styles.modalInputBox} mt-3 mb-3`}>
+                            <label htmlFor="">New password</label>
+                            <input type="text" placeholder="Confirm new password" />
+                        </div>
+                        <p style={{ fontSize: "12px", color: "#6A6A6A" }}>Password must be between 8-20 characters, and include at least one lowercase letter, once UPPERCASE letter, and one number</p>
+                        <div className="mt-4">
+                            <button className="my-btn w-100" onClick={() => setShowModalMd("authorization")}>Change Password</button>
+                        </div>
+                    </div>}
+                    {showModalMd === "authorization" && <div className={styles.modalLgBox}>
+                        <h2>VEHDE</h2>
+                        <p style={{ textAlign: "center" }}>An Authorization code was sent to your mobile phone number</p>
+                        <h4 style={{ textAlign: "center" }}>XXX-XXX-1984</h4>
+                        <div></div>
+                        <div className={`${styles.modalInputBox} mt-5`}>
+                            <label htmlFor="">VEHDE Password</label>
+                            <input type="text" placeholder="Enter current password" />
+                        </div>
+                        <div className="mt-3">
+                            <button className="my-btn w-100" onClick={() => setShowModalMd("password changed")}>Change Password</button>
+                        </div>
+                        <p style={{ fontSize: "12px", color: "#6A6A6A", textAlign: "center", marginTop: "20px", cursor: "pointer" }}>Request another authorization code</p>
+                    </div>}
+                    {showModalMd === "password changed" && <div className={`${styles.modalLgBox} text-center`}>
+                        <p style={{ textAlign: "center", marginTop: "70px" }}>Your password was successfully changed.</p>
+                        <i className="fa fa-check" style={{ fontSize: "120px", textAlign: "center", color: "#DE4F4D", marginBottom: "60px" }}></i>
+                    </div>}
+                </ModalMd>
             }
         </>
     )
